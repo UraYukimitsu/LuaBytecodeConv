243bd18534ffefcef480cabde16a9c6b9eeaa782
UraYukimitsu <>
2017-01-21 02:05:17 +0100

    Update luacOutParse.py

--- luacOutParse.py
+++ luacOutParse.py
@@ -7,9 +7,7 @@ if len(sys.argv) > 1:
 	lines = textfile.readlines()
 	textfile.close()
 	for i in lines:
-		#i = i + "eol"
 		j = re.sub(r"\t?[0-9]+\t+[][-]+\t([A-Z]+[ ]+?)\t?((-?[0-9]+ ?)+)(\t;)?(.+?)?\n", r"\1 \2", i)
 		if i != j:
-			i = os.popen('lbc52 ' + j).read()[:-1] + " | " + i[:-1] #+ "  --  " + j 
-		#print(i[:-1])
-		print(i)
+			i = os.popen('lbc52 ' + j).read()[:-1] + " | " + i
+		print(i[:-1])
